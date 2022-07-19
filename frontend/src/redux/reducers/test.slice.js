307241f6a6accf2ad14feb7ebd5d45b4b4816aea
Allan Chauvin <chauvin.allan1997@outlook.fr>
2022-07-19 13:12:00 +0200

    Initial commit

---
+++ frontend/src/redux/reducers/test.slice.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-empty-pattern */
+import { createSlice } from "@reduxjs/toolkit";
+
+const testSlice = createSlice({
+  name: 'todoList',
+  initialState: {
+
+  },
+  reducers: {
+    
+  },
+  extraReducers: {
+    
+  }
+})
+
+// Action
+export const {
+
+} = testSlice.actions;
+
+//State
+
+
+//Reducer
+export default testSlice.reducer;
